39090601a22d7754a951ea0292c9c0c6dc48df71
Andres <jaime-aranda@outlook.com>
2021-06-28 00:50:35 -0500

    change uri if project is not in root directory

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
 //configure router
-$uri = Helper::uri_remove_first(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$uri = $_ENV['BASE_DIR'] ? Helper::uri_remove_first($uri) : $uri;
 $verb = $_SERVER['REQUEST_METHOD'];
 $router = new Router($uri, $verb);
